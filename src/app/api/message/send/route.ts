300378053f4eb05d3731b65ee7c05df41132bea0
Deep6239 <110710828+Deep6239@users.noreply.github.com>
2023-12-15 22:17:57 +0530

    Updated route.ts

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -48,9 +48,9 @@ export async function POST(req: Request) {
 
         const message = messageValidator.parse(messageData)
 
-        pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
+        await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
 
-        pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {...message, senderImg: sender.image, senderName: sender.name})
+        await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {...message, senderImg: sender.image, senderName: sender.name})
 
         // all valid, send the message
         await db.zadd(`chat:${chatId}:messages`, {
